5743b358945da024f4fa9dffed6550932c7658ba
LEe5512 <140491512+LEe5512@users.noreply.github.com>
2025-03-30 22:36:20 +1030

    get response from the origin server, send it to client, save origin server response to cache file

--- Proxy-skeleton(2025).py
+++ Proxy-skeleton(2025).py
@@ -173,10 +173,12 @@ while True:
 
       # Get the response from the origin server
       # ~~~~ INSERT CODE ~~~~
+      response_bytes = originServerSocket.recv(BUFFER_SIZE)
       # ~~~~ END CODE INSERT ~~~~
 
       # Send the response to the client
       # ~~~~ INSERT CODE ~~~~
+      clientSocket.sendall(response_bytes)
       # ~~~~ END CODE INSERT ~~~~
 
       # Create a new file in the cache for the requested file.
@@ -188,6 +190,7 @@ while True:
 
       # Save origin server response in the cache file
       # ~~~~ INSERT CODE ~~~~
+      cacheFile.write(response_bytes)
       # ~~~~ END CODE INSERT ~~~~
       cacheFile.close()
       print ('cache file closed')
